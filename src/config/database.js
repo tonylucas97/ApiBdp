2a0adcdb1502013236440385c34e6906adbb2935
tony Lucas <tonyjogos123@gmail.com>
2021-06-03 21:48:52 -0300

    asdasd

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("bpdapidb","tonyjogos123","55425610a",{
-    host:"mysql743.umbler.com",
+const sequelize = new Sequelize("heroku_b14fb5a2189fe8","bc7b1e9e9f5739","a156d1f9",{
+    host:"us-cdbr-east-04.cleardb.com",
     dialect:"mysql",
     raw:true,
     define:{
